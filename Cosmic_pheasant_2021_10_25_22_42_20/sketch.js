e84a9ac10bbccc2560bbd4f01e2cef69f0cf1539
Kim Nguyen <kkn2011@nyu.edu>
2021-10-25 19:27:30 -0400

    testing
    I'm testing out how to do class on p5 and starting off with a simple circle so I have a reference

--- Cosmic_pheasant_2021_10_25_22_42_20/sketch.js
+++ Cosmic_pheasant_2021_10_25_22_42_20/sketch.js
@@ -1,7 +1,28 @@
+let toy; 
+
 function setup() {
   createCanvas(400, 400);
+  toy = new Toy(); 
+
 }
 
 function draw() {
   background(220);
+  toy.display(); 
+
+}
+
+
+//class for toy 
+class Toy {
+  constructor(){
+    this.x = 50; 
+    this.y = 50; 
+    this.c = color(255);
+  }
+
+  display(){
+    ellipse(50, 50, 10, 10); 
+  }
+
 }
