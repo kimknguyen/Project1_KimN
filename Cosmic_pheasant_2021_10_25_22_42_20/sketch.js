54ea6a6917ac06852dfd8acfd4841bf1de7f8e77
Kim Nguyen <kkn2011@nyu.edu>
2021-10-28 19:04:32 -0400

    Display function for toy
    I was able to make a visual for the toy horse and I plan to start adding parameters and variables to my class.

--- Cosmic_pheasant_2021_10_25_22_42_20/sketch.js
+++ Cosmic_pheasant_2021_10_25_22_42_20/sketch.js
@@ -1,7 +1,7 @@
 let toy; 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   toy = new Toy(); 
 
 }
@@ -22,7 +22,39 @@ class Toy {
   }
 
   display(){
-    ellipse(50, 50, 10, 10); 
+    stroke(0); 
+    strokeWeight(2); 
+
+    //body 
+    fill(255); 
+    beginShape(); 
+    vertex(225,190); 
+    vertex(190, 280); 
+    vertex(190, 300); 
+    vertex(250, 300); 
+    endShape(CLOSE); 
+
+    //head 
+    fill(255); //change color
+    ellipse(190, 152, 30, 50); //ear behind  
+    bezier(212, 152, -26, 237, 153, 354, 213, 255); 
+    //line(212, 152, 215, 253); 
+    bezier(212, 152, 240, 180, 220, 250, 213, 255 )
+    ellipseMode(CENTER); 
+    ellipse(215, 152, 30, 50); //ear in front
+    strokeWeight(7); 
+    stroke(0);
+    bezier(125, 200, 180, 250, 185, 270, 190, 280); //horse strap
+    strokeWeight(2); 
+    stroke(0); 
+    ellipseMode(CENTER); 
+    ellipse(190, 200, 10, 10); //eye
+    ellipse(120, 250, 10, 20); //nose
+
+    //stick 
+    rect(215, 300, 20, 100); 
+
+
   }
 
 }
